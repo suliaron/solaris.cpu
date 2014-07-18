097f4513228bed9c952e5dcf0b7c8389c4945df1
MartonGeda <gedapepe@gmail.com>
2014-07-18 10:51:35 +0200

    Initial check-in

---
+++ src/Solaris.cpu/Common.h
@@ -0,0 +1,19 @@
+typedef enum integrator{
+	INTEGRATOR_RK4,
+	INTEGRATOR_RK56,
+	INTEGRATOR_N
+}integrator_t;
+
+typedef enum frame_center{
+	FRAME_CENTER_ASTRO,
+	FRAME_CENTER_BARY,
+	FRAME_CENTER_N
+}frame_center_t;
+
+typedef struct settings{
+	bool enableDistinctStartTimes;
+	bool barycentric;
+	integrator_t inttype;
+	frame_center_t frame_center;
+
+}settings_t;
